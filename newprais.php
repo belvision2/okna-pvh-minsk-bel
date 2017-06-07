f7b0066543888163e5c4578ac4ccdb4bcc94e085
sergeigaev <belvision2@users.noreply.github.com>
2017-06-07 18:16:33 +0300

    до внедрения php

---
+++ newprais.php
@@ -0,0 +1,329 @@
+<?php 
+$data_prais_26 = htmlspecialchars($_POST['data_prais_26'], NULL, 'utf-8');
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_26)) { }
+else {
+$data = '$prais_new_026 = '.$data_prais_26.';' . "\n";//В виндовс не \n, а \r\n
+$file = 'prais.php';
+$array=file( $file ); //файл в котором надо изменить строку
+$array[7]=$data; //строка в которой нужно изменить значение $data
+file_put_contents( $file, $array );}
+
+
+$data_prais_27 = htmlspecialchars($_POST['data_prais_27'], NULL, 'utf-8');
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_27)) {}
+else {
+$data2 = '$prais_new_027 = '.$data_prais_27.';' . "\n";//В виндовс не \n, а \r\n
+$file2 = 'prais.php';
+$array2=file( $file2 ); //файл в котором надо изменить строку
+$array2[8]=$data2; //строка в которой нужно изменить значение $data
+file_put_contents( $file2, $array2 );}
+
+
+
+$data_prais_28 = htmlspecialchars($_POST['data_prais_28'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_28)) {}                        // 1
+else {
+$data3 = '$prais_new_028 = '.$data_prais_28.';' . "\n";                    // 3  
+$file3 = 'prais.php';                                                      // 1
+$array3=file( $file3 ); //файл в котором надо изменить строку                 2
+$array3[9]=$data3; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file3, $array3 );}                                        
+
+
+
+$data_prais_29 = htmlspecialchars($_POST['data_prais_29'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_29)) {}                        // 1
+else {
+$data4 = '$prais_new_029 = '.$data_prais_29.';' . "\n";                    // 3  
+$file4 = 'prais.php';                                                      // 1
+$array4=file( $file4 ); //файл в котором надо изменить строку                 2
+$array4[10]=$data4; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file4, $array4 );}                                        
+
+
+
+$data_prais_30 = htmlspecialchars($_POST['data_prais_30'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_30)) {}                        // 1
+else {
+$data5 = '$prais_new_030 = '.$data_prais_30.';' . "\n";                    // 3  
+$file5 = 'prais.php';                                                      // 1
+$array5=file( $file5 ); //файл в котором надо изменить строку                 2
+$array5[11]=$data5; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file5, $array5 );}                                        
+
+
+
+$data_prais_31 = htmlspecialchars($_POST['data_prais_31'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_31)) {}                        // 1
+else {
+$data500 = '$prais_new_031 = '.$data_prais_31.';' . "\n";                    // 3  
+$file500 = 'prais.php';                                                      // 1
+$array500=file( $file500 ); //файл в котором надо изменить строку                 2
+$array500[12]=$data500; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file500, $array500 );}                                        
+
+
+
+$data_prais_32 = htmlspecialchars($_POST['data_prais_32'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_32)) {}                        // 1
+else {
+$data6 = '$prais_new_032 = '.$data_prais_32.';' . "\n";                    // 3  
+$file6 = 'prais.php';                                                      // 1
+$array6=file( $file6 ); //файл в котором надо изменить строку                 2
+$array6[13]=$data6; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file6, $array6 );}                                        
+
+
+
+$data_prais_33 = htmlspecialchars($_POST['data_prais_33'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_33)) {}                        // 1
+else {
+$data7 = '$prais_new_033 = '.$data_prais_33.';' . "\n";                    // 3  
+$file7 = 'prais.php';                                                      // 1
+$array7=file( $file7 ); //файл в котором надо изменить строку                 2
+$array7[14]=$data7; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file7, $array7 );}                                        
+
+
+
+
+$data_prais_34 = htmlspecialchars($_POST['data_prais_34'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_34)) {}                        // 1
+else {
+$data8 = '$prais_new_034 = '.$data_prais_34.';' . "\n";                    // 3  
+$file8 = 'prais.php';                                                      // 1
+$array8=file( $file8 ); //файл в котором надо изменить строку                 2
+$array8[15]=$data8; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file8, $array8 );}                                        
+
+
+
+$data_prais_35 = htmlspecialchars($_POST['data_prais_35'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_35)) {}                        // 1
+else {
+$data9 = '$prais_new_035 = '.$data_prais_35.';' . "\n";                    // 3  
+$file9 = 'prais.php';                                                      // 1
+$array9=file( $file9 ); //файл в котором надо изменить строку                 2
+$array9[16]=$data9; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file9, $array9 );}                                        
+
+
+
+$data_prais_36 = htmlspecialchars($_POST['data_prais_36'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_36)) {}                        // 1
+else {
+$data10 = '$prais_new_036 = '.$data_prais_36.';' . "\n";                    // 3  
+$file10 = 'prais.php';                                                      // 1
+$array10=file( $file10 ); //файл в котором надо изменить строку                 2
+$array10[17]=$data10; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file10, $array10 );}                                        
+
+
+
+$data_prais_37 = htmlspecialchars($_POST['data_prais_37'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_37)) {}                        // 1
+else {
+$data11 = '$prais_new_037 = '.$data_prais_37.';' . "\n";                    // 3  
+$file11 = 'prais.php';                                                      // 1
+$array11=file( $file11 ); //файл в котором надо изменить строку                 2
+$array11[18]=$data11; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file11, $array11 );}                                        
+
+
+
+$data_prais_38 = htmlspecialchars($_POST['data_prais_38'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_38)) {}                        // 1
+else {
+$data12 = '$prais_new_038 = '.$data_prais_38.';' . "\n";                    // 3  
+$file12 = 'prais.php';                                                      // 1
+$array12=file( $file12 ); //файл в котором надо изменить строку                 2
+$array12[19]=$data12; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file12, $array12 );}                                        
+
+
+
+$data_prais_39 = htmlspecialchars($_POST['data_prais_39'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_39)) {}                        // 1
+else {
+$data13 = '$prais_new_039 = '.$data_prais_39.';' . "\n";                    // 3  
+$file13 = 'prais.php';                                                      // 1
+$array13=file( $file13 ); //файл в котором надо изменить строку                 2
+$array13[20]=$data13; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file13, $array13 );}                                        
+
+
+$data_prais_40 = htmlspecialchars($_POST['data_prais_40'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_40)) {}                        // 1
+else {
+$data14 = '$prais_new_040 = '.$data_prais_40.';' . "\n";                    // 3  
+$file14 = 'prais.php';                                                      // 1
+$array14=file( $file14 ); //файл в котором надо изменить строку                 2
+$array14[21]=$data14; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file14, $array14 );}                                        
+
+
+
+$data_prais_41 = htmlspecialchars($_POST['data_prais_41'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_41)) {}                        // 1
+else {
+$data15 = '$prais_new_041 = '.$data_prais_41.';' . "\n";                    // 3  
+$file15 = 'prais.php';                                                      // 1
+$array15=file( $file15 ); //файл в котором надо изменить строку                 2
+$array15[22]=$data15; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file15, $array15 );}                                        
+
+
+
+$data_prais_42 = htmlspecialchars($_POST['data_prais_42'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_42)) {}                        // 1
+else {
+$data16 = '$prais_new_042 = '.$data_prais_42.';' . "\n";                    // 3  
+$file16 = 'prais.php';                                                      // 1
+$array16=file( $file16 ); //файл в котором надо изменить строку                 2
+$array16[23]=$data16; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file16, $array16 );}                                        
+
+
+
+$data_prais_43 = htmlspecialchars($_POST['data_prais_43'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_43)) {}                        // 1
+else {
+$data17 = '$prais_new_043 = '.$data_prais_43.';' . "\n";                    // 3  
+$file17 = 'prais.php';                                                      // 1
+$array17=file( $file17 ); //файл в котором надо изменить строку                 2
+$array17[24]=$data17; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file17, $array17 );}                                        
+
+
+
+$data_prais_44 = htmlspecialchars($_POST['data_prais_44'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_44)) {}                        // 1
+else {
+$data18 = '$prais_new_044 = '.$data_prais_44.';' . "\n";                    // 3  
+$file18 = 'prais.php';                                                      // 1
+$array18=file( $file18 ); //файл в котором надо изменить строку                 2
+$array18[25]=$data18; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file18, $array18 );}                                        
+
+
+
+$data_prais_45 = htmlspecialchars($_POST['data_prais_45'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_45)) {}                        // 1
+else {
+$data19 = '$prais_new_045 = '.$data_prais_45.';' . "\n";                    // 3  
+$file19 = 'prais.php';                                                      // 1
+$array19=file( $file19 ); //файл в котором надо изменить строку                 2
+$array19[26]=$data19; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file19, $array19 );}                                        
+
+
+
+$data_prais_46 = htmlspecialchars($_POST['data_prais_46'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_46)) {}                        // 1
+else {
+$data20 = '$prais_new_046 = '.$data_prais_46.';' . "\n";                    // 3  
+$file20 = 'prais.php';                                                      // 1
+$array20=file( $file20 ); //файл в котором надо изменить строку                 2
+$array20[27]=$data3; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file20, $array20 );}                                        
+
+
+
+$data_prais_47 = htmlspecialchars($_POST['data_prais_47'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_47)) {}                        // 1
+else {
+$data21 = '$prais_new_047 = '.$data_prais_47.';' . "\n";                    // 3  
+$file21 = 'prais.php';                                                      // 1
+$array21=file( $file21 ); //файл в котором надо изменить строку                 2
+$array21[28]=$data21; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file21, $array21 );}                                        
+
+
+
+$data_prais_48 = htmlspecialchars($_POST['data_prais_48'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_48)) {}                        // 1
+else {
+$data22 = '$prais_new_048 = '.$data_prais_48.';' . "\n";                    // 3  
+$file22 = 'prais.php';                                                      // 1
+$array22=file( $file22 ); //файл в котором надо изменить строку                 2
+$array22[29]=$data22; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file22, $array22 );}                                        
+
+
+
+$data_prais_49 = htmlspecialchars($_POST['data_prais_49'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_49)) {}                        // 1
+else {
+$data23 = '$prais_new_049 = '.$data_prais_49.';' . "\n";                    // 3  
+$file23 = 'prais.php';                                                      // 1
+$array23=file( $file23 ); //файл в котором надо изменить строку                 2
+$array23[30]=$data23; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file23, $array23 );}                                        
+
+
+
+$data_prais_50 = htmlspecialchars($_POST['data_prais_50'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_50)) {}                        // 1
+else {
+$data24 = '$prais_new_050 = '.$data_prais_50.';' . "\n";                    // 3  
+$file24 = 'prais.php';                                                      // 1
+$array24=file( $file24 ); //файл в котором надо изменить строку                 2
+$array24[31]=$data24; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file24, $array24 );}                                        
+
+
+
+$data_prais_51 = htmlspecialchars($_POST['data_prais_51'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_51)) {}                        // 1
+else {
+$data25 = '$prais_new_051 = '.$data_prais_51.';' . "\n";                    // 3  
+$file25 = 'prais.php';                                                      // 1
+$array25=file( $file25 ); //файл в котором надо изменить строку                 2
+$array25[32]=$data25; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file25, $array25 );}                                        
+
+
+
+$data_prais_52 = htmlspecialchars($_POST['data_prais_52'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_52)) {}                      
+else {
+$data26 = '$prais_new_052 = '.$data_prais_52.';' . "\n";                    // 3  
+$file26 = 'prais.php';                                                      // 1
+$array26=file( $file26 ); //файл в котором надо изменить строку                 2
+$array26[33]=$data26; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file26, $array26 );}                                       
+
+
+
+$data_prais_53 = htmlspecialchars($_POST['data_prais_53'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_53)) {}                        
+else {
+$data27 = '$prais_new_053 = '.$data_prais_53.';' . "\n";                    // 3  
+$file27 = 'prais.php';                                                      // 1
+$array27=file( $file27 ); //файл в котором надо изменить строку                 2
+$array27[34]=$data27; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file27, $array27 );}                                        
+
+
+
+$data_prais_54 = htmlspecialchars($_POST['data_prais_54'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_54)) {}                      
+else {
+$data28 = '$prais_new_054 = '.$data_prais_54.';' . "\n";                    // 3  
+$file28 = 'prais.php';                                                      // 1
+$array28=file( $file28 ); //файл в котором надо изменить строку                 2
+$array28[35]=$data28; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file28, $array28 );}                                        
+
+
+
+$data_prais_55 = htmlspecialchars($_POST['data_prais_55'], NULL, 'utf-8'); // 2 замены
+if(!preg_match("/^[0-9]{2,4}$/",$data_prais_55)) {}                      
+else {
+$data29 = '$prais_new_055 = '.$data_prais_55.';' . "\n";                    // 3  
+$file29 = 'prais.php';                                                      // 1
+$array29=file( $file29 ); //файл в котором надо изменить строку                 2
+$array29[9]=$data29; //строка в которой нужно изменить значение $data           3
+file_put_contents( $file29, $array29 );}                                        
+header("Location: prais.html");
+?>
