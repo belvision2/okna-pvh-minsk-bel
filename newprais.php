458f1244e92c1cdb153d30034cb4cc3357adcd36
sergeigaev <belvision2@users.noreply.github.com>
2017-06-07 21:09:50 +0300

    добавлены все телефоны в переменные

--- newprais.php
+++ newprais.php
@@ -1,4 +1,14 @@
-<?php 
+<?php
+$data_tell_velcom2 = htmlspecialchars($_POST['data_tell_velcom2'], NULL, 'utf-8');
+if(!preg_match("/^[0-9-+ ]{17}$/",$data_tell_velcom2)) { }
+else {
+$data2 = '$tell_velcom2 = "'.$data_tell_velcom2.'";' . "\n";//В виндовс не \n, а \r\n
+$file2 = 'prais.php';
+$array2=file( $file2 ); //файл в котором надо изменить строку
+$array2[2]=$data2; //строка в которой нужно изменить значение $data
+file_put_contents( $file2, $array2 );}
+
+
 $data_tell_velcom1 = htmlspecialchars($_POST['data_tell_velcom1'], NULL, 'utf-8');
 if(!preg_match("/^[0-9-+ ]{17}$/",$data_tell_velcom1)) { }
 else {
@@ -7,5 +17,19 @@ $file = 'prais.php';
 $array=file( $file ); //файл в котором надо изменить строку
 $array[1]=$data; //строка в которой нужно изменить значение $data
 file_put_contents( $file, $array );}
+
+$data_tell_mts = htmlspecialchars($_POST['data_tell_mts'], NULL, 'utf-8');
+if(!preg_match("/^[0-9-+ ]{17}$/",$data_tell_mts)) { }
+else {
+$data3 = '$tell_mts = "'.$data_tell_mts.'";' . "\n";//В виндовс не \n, а \r\n
+$file3 = 'prais.php';
+$array3=file( $file3 ); //файл в котором надо изменить строку
+$array3[3]=$data3; //строка в которой нужно изменить значение $data
+file_put_contents( $file3, $array3 );}
+
+
+
+
+
 header("Location: prais.html");
 ?>
